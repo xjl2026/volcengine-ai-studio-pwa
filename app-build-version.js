12db61291c8f564b9123d82a74c10c226cb90785
xjl2026 <yalixjl@gmail.com>
2026-08-17 11:37:13 +0800

    bump app display version to 1.7.15

--- app-build-version.js
+++ app-build-version.js
@@ -1,7 +1,7 @@
-// App build version display - v1.7.14
+// App build version display - v1.7.15
 (function () {
   'use strict';
-  const APP_DISPLAY_VERSION = '1.7.14';
+  const APP_DISPLAY_VERSION = '1.7.15';
 
   function applyVersion() {
     const el = document.getElementById('versionText');
